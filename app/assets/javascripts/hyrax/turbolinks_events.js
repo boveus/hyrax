ca082edbf30f407b25a5e955e2fcc397120fb625
Chris Colvard <cjcolvar@indiana.edu>
2018-05-02 17:12:50 -0400

    Add popstate event handler to cover the case where the browser history state is missing a turbolinks object due to Universal Viewer's manipulation of the browser history outside of turbolinks.  This fix is based on https://github.com/turbolinks/turbolinks/issues/219#issuecomment-275838923.

--- app/assets/javascripts/hyrax/turbolinks_events.js
+++ app/assets/javascripts/hyrax/turbolinks_events.js
@@ -6,3 +6,20 @@ $(document).on('turbolinks:load', function() {
   // See https://github.com/samvera/hyrax/issues/2906
   window.embedScriptIncluded = false;
 });
+
+// Fixes a back/forward navigation problem with UV and turbolinks
+// See https://github.com/samvera/hyrax/issues/2964
+// This is based on https://github.com/turbolinks/turbolinks/issues/219#issuecomment-275838923
+$(window).on('popstate', function(event) {
+  var turbolinks_location = Turbolinks.Location.wrap(window.location);
+  if (Turbolinks.controller.location.requestURL === turbolinks_location.requestURL) {
+    return;
+  }
+  if (event.state === undefined || event.state.turbolinks === undefined) {
+    if (window.history.state !== null && window.history.state.turbolinks !== undefined) {
+      Turbolinks.controller.historyPoppedToLocationWithRestorationIdentifier(turbolinks_location, window.history.state.turbolinks.restorationIdentifier);
+    } else {
+      Turbolinks.controller.historyPoppedToLocationWithRestorationIdentifier(turbolinks_location, Turbolinks.uuid());
+    }
+  }
+});
